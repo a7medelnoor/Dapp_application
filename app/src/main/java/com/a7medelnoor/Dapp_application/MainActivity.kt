7328fc0be8f0bf2b832948eca273c328f2ae1e12
ahmed <acc.ahmedstudio@gmail.com>
2023-03-04 02:27:30 +0400

    add button connect to wallet and create connect to wallet function

--- app/src/main/java/com/a7medelnoor/Dapp_application/MainActivity.kt
+++ app/src/main/java/com/a7medelnoor/Dapp_application/MainActivity.kt
@@ -2,10 +2,22 @@ package com.a7medelnoor.Dapp_application
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    lateinit var buttonConnectWallet : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        buttonConnectWallet = findViewById(R.id.connect_metamsk)
+       // connect metamask
+        buttonConnectWallet.setOnClickListener{
+          connectWalletMetaMask();
+        }
+        // connect wallet
+    }
+    fun connectWalletMetaMask(){
+
     }
 }
