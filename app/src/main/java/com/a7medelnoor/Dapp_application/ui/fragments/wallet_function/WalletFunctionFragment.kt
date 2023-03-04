ab3db5e96090a71c6d27040448ccd1dc12efe266
ahmed <acc.ahmedstudio@gmail.com>
2023-03-04 21:55:21 +0400

    view model get wallet account

--- app/src/main/java/com/a7medelnoor/Dapp_application/ui/fragments/wallet_function/WalletFunctionFragment.kt
+++ app/src/main/java/com/a7medelnoor/Dapp_application/ui/fragments/wallet_function/WalletFunctionFragment.kt
@@ -1,13 +1,12 @@
 package com.a7medelnoor.Dapp_application.ui.fragments.wallet_function
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import com.a7medelnoor.Dapp_application.R
-import com.a7medelnoor.Dapp_application.databinding.FragmentConnectingWalletBinding
 import com.a7medelnoor.Dapp_application.databinding.FragmentWalletFunctionBinding
 import com.a7medelnoor.Dapp_application.ui.viewmodel.ConnectWalletViewModel
 
@@ -18,23 +17,29 @@ class WalletFunctionFragment : Fragment() {
 
     private val binding get() = _binding!!
     private val viewmodel: ConnectWalletViewModel by activityViewModels()
-
+    private val TAG = "WalletFunctionFragment"
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        _binding = FragmentWalletFunctionBinding.inflate(inflater,container,false)
+        _binding = FragmentWalletFunctionBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewmodel.getAccounts().observe(viewLifecycleOwner) {
+            Log.d(TAG, "Account: $it")
+            binding.walletBalance.text = it.toString()
+
+        }
 
     }
+
     override fun onDestroyView() {
         super.onDestroyView()
-        _binding=null
+        _binding = null
     }
 
 }
