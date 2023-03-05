220b0f86e9c5d475902fd499ced47f2ca2bfe790
ahmed <acc.ahmedstudio@gmail.com>
2023-03-05 13:08:10 +0400

    nfc status

---
+++ app/src/main/java/com/a7medelnoor/Dapp_application/app/NFCStatus.kt
@@ -0,0 +1,12 @@
+package com.a7medelnoor.Dapp_application.app
+
+enum class NFCStatus {
+    NoOperation,
+    Tap,
+    Process,
+    Confirmation,
+    Read,
+    Write,
+    NotSupported,
+    NotEnabled,
+}
